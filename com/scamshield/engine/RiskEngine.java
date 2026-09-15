624da9d3a4bc172a9d8a69f133e97387ceb1987a
pariporwal27 <pari.porwal.27@gmail.com>
2026-09-15 21:37:26 +0530

    Create RiskEngine.java

---
+++ com/scamshield/engine/RiskEngine.java
@@ -0,0 +1,45 @@
+package com.scamshield.engine;
+
+import com.scamshield.model.ScanRequest;
+import com.scamshield.model.ScanResult;
+import com.scamshield.rule.ScamRule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Core engine that aggregates scam detection rules and computes total risk score.
+ */
+public class RiskEngine {
+    private final List<ScamRule> rules;
+
+    public RiskEngine() {
+        this.rules = new ArrayList<>();
+    }
+
+    public void registerRule(ScamRule rule) {
+        if (rule != null) {
+            this.rules.add(rule);
+        }
+    }
+
+    public ScanResult analyze(ScanRequest request) {
+        int totalScore = 0;
+        List<String> triggeredRules = new ArrayList<>();
+
+        for (ScamRule rule : rules) {
+            int scoreContribution = rule.evaluate(request);
+            if (scoreContribution > 0) {
+                totalScore += scoreContribution;
+                triggeredRules.add(rule.getRuleName() + ": " + rule.getTriggerDescription());
+            }
+        }
+
+        return new ScanResult(request.getRequestId(), totalScore, triggeredRules);
+    }
+
+    public List<ScamRule> getRegisteredRules() {
+        return Collections.unmodifiableList(rules);
+    }
+}
